17a0346abe139297b1506c9df61f2d7281027420
Laura Gwen <lauraburch.lb@gmail.com>
2020-11-23 00:56:29 -0500

    Added 5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -19,12 +19,13 @@ $(document).ready(function(){
     var upperCaseCity = cityName.toUpperCase(); 
     // THEN I get the value that is entered into the search input
     makeWeatherRequest(upperCaseCity);
-    var time = moment().format("MMM Do YYYY"); 
+    var time = moment().format("(MMM Do YYYY)"); 
     $("#currentDay").text(time);
     $("#cityNameHere").text(upperCaseCity);
     previousCities.push(cityName);
     localStorage.setItem("city", JSON.stringify(previousCities))
     renderButton()
+    
   }
      
   function renderButton(){
@@ -73,32 +74,86 @@ $(document).ready(function(){
       $("#temperature").text("Temperature: " + tempChange + " °F");
       
     $.ajax( queryUV ).then(function(response) {
-        // console.log(response)
+        console.log(response)
         var uvIndex=response.current.uvi
         $("#uvIndex").text("UV Index: " + uvIndex)
        
-      // START rendering data to the HTML
+        for(var i = 0; i < uvIndex; i++){
+          
+          if (uvIndex < 3 ){
+            $("#uvIndex").addClass("moderate");
+          }
+          else if (uvIndex < 5 ){
+            $("#uvIndex").addClass("high")
+          }
+          else if (uvIndex > 7 ){
+            $("#uvIndex").addClass("veryHigh")
+          }
+          else if (uvIndex > 10 ) {
+            $("#uvIndex").addClass("extreme")
+          }
+       }
+      
+       var daily = moment().add(1, 'days').calendar("(MMM Do YYYY)");   
+       var iconDayOne=response.daily[1].weather.icon
+       var tempOne=response.daily[1].temp.day
+       var tempOneNew=Math.floor((tempOne-273.15)*1.8)+32
+       $("#fiveDay").text(daily);
+       $("#dayIcon").attr("src", "http://openweathermap.org/img/w/" + iconDayOne + ".png")
+       $("#dayTemp").text("Temp: " + tempOneNew + " °F");
+       $("#dayHumidity").text("Humidity: " + response.daily[1].humidity + " %")
 
-      // Then get the lat and log out of thr 'response object'
+       var daily2 = moment().add(2, 'days').calendar("(MMM Do YYYY)");   
+       var iconDayOne2=response.daily[2].weather.icon
+       var tempOne2=response.daily[2].temp.day
+       var tempOneNew2=Math.floor((tempOne2-273.15)*1.8)+32
+       $("#fiveDay2").text(daily2);
+       $("#dayIcon2").attr("src", "http://openweathermap.org/img/w/" + iconDayOne2 + ".png")
+       $("#dayTemp2").text("Temp: " + tempOneNew2 + " °F");
+       $("#dayHumidity2").text("Humidity: " + response.daily[2].humidity + " %")
 
-      // NEXT call"makeOneCallRequest( lat, lng )' and pass in the lat and lng.
-    // });
+       var daily3 = moment().add(3, 'days').calendar("(MMM Do YYYY)");   
+       var iconDayOne3=response.daily[3].weather.icon
+       var tempOne3=response.daily[3].temp.day
+       var tempOneNew3=Math.floor((tempOne3-273.15)*1.8)+32
+       $("#fiveDay3").text(daily3);
+       $("#dayIcon3").attr("src", "http://openweathermap.org/img/w/" + iconDayOne3 + ".png")
+       $("#dayTemp3").text("Temp: " + tempOneNew3 + " °F");
+       $("#dayHumidity3").text("Humidity: " + response.daily[3].humidity + " %")
+
+       var daily4 = moment().add(4, 'days').calendar("(MMM Do YYYY)");   
+       var iconDayOne4=response.daily[4].weather.icon
+       var tempOne4=response.daily[4].temp.day
+       var tempOneNew4=Math.floor((tempOne4-273.15)*1.8)+32
+       $("#fiveDay4").text(daily4);
+       $("#dayIcon4").attr("src", "http://openweathermap.org/img/w/" + iconDayOne4 + ".png")
+       $("#dayTemp4").text("Temp: " + tempOneNew4 + " °F");
+       $("#dayHumidity4").text("Humidity: " + response.daily[4].humidity + " %")
+
+       var daily5 = moment().add(5, 'days').calendar("(MMM Do YYYY)");   
+       var iconDayOne5=response.daily[5].weather.icon
+       var tempOne5=response.daily[5].temp.day
+       var tempOneNew5=Math.floor((tempOne5-273.15)*1.8)+32
+       $("#fiveDay5").text(daily5);
+       $("#dayIcon5").attr("src", "http://openweathermap.org/img/w/" + iconDayOne5 + ".png")
+       $("#dayTemp5").text("Temp: " + tempOneNew5 + " °F");
+       $("#dayHumidity5").text("Humidity: " + response.daily[5].humidity + " %")
     });
   })
 }
 
-  function makeOneCallRequest ( lat, lng ){
+  // function makeOneCallRequest ( lat, lng ){
 
     // NEXT, build the url for 1st api request 
     // 
 
     // next, make the rrequest to the url with jquery ajax
-    $.ajax( queryURL ).then(function(response) {
+    // $.ajax( queryURL ).then(function(response) {
 
       // finish rendering data to the html
 
 
-  });
+  // });
 
 
   //   $.ajax({
@@ -111,7 +166,7 @@ $(document).ready(function(){
 
 
 
-  }
+  // }
       
   
   // var clearButton = $("#button")
@@ -128,6 +183,7 @@ $(document).ready(function(){
   $(searchButton).on("click",function(e){
     e.preventDefault();
     handleSearch();
+
   });
 
  
